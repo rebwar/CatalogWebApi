bac53ff898706db3ee43ca40fd34b9f68390c4b6
Rebwar Hosein Poori <rebwar.poori@hotmail.com>
2022-02-28 19:05:37 +0330

    Some fixes

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemController:ControllerBase
+    {
+        
+    }
+}
