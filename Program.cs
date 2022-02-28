aebb8edd8679cb3bdcc1a0e8d81cae7a204f6b1f
Rebwar Hosein Poori <rebwar.poori@hotmail.com>
2022-02-28 19:48:50 +0330

    Add Service for ItemRepository

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 global using Catalog.Entities;
+using Catalog.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<IItemRepository,InMemItemRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
